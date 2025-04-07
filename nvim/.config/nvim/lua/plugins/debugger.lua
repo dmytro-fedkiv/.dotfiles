436da3bfd9839d7fe1bba21db7a7ba06e6ecad8f
dmytrofedkiv <dmytro.fedkiv@icloud.com>
2025-04-06 20:14:20 -0400

    Add debugger.lua

---
+++ nvim/.config/nvim/lua/plugins/debugger.lua
@@ -0,0 +1,50 @@
+return {
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		"rcarriga/nvim-dap-ui",
+		"nvim-neotest/nvim-nio",
+	},
+	config = function()
+		local dap = require("dap")
+
+		dap.adapters["pwa-node"] = {
+			type = "server",
+			host = "127.0.0.1",
+			port = 8123,
+			executable = {
+				command = "js-debug-adapter",
+			},
+		}
+
+		for _, language in ipairs({ "typescript", "javascript" }) do
+			dap.configurations[language] = {
+				{
+					type = "pwa-node",
+					request = "launch",
+					name = "Launch file",
+					program = "${file}",
+					cwd = "${workspaceFolder}",
+					runtimeExecutable = "node",
+				},
+			}
+		end
+
+		vim.keymap.set("n", "<leader>dt", dap.toggle_breakpoint, {})
+		vim.keymap.set("n", "<leader>dc", dap.continue, {})
+
+		local dapui = require("dapui")
+
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.close()
+		end
+	end,
+}
