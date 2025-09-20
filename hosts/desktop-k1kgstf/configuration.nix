f171575192d694cc867cfb5ba92cbfa5c673b114
Dmytro Fedkiv <dmytro.fedkiv@icloud.com>
2025-09-19 22:56:06 -0400

    Add desktop-k1kgstf host

---
+++ hosts/desktop-k1kgstf/configuration.nix
@@ -0,0 +1,38 @@
+{ inputs, pkgs, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+
+    ../../modules/home-manager/desktop/hyprland.nix
+
+    ../../modules/home-manager/code-editor/nvim.nix
+
+    ../../modules/home-manager/terminal/tmux.nix
+    ../../modules/home-manager/terminal/zsh.nix
+
+    ../../modules/nixos/boot.nix
+    ../../modules/nixos/network.nix
+    ../../modules/nixos/nixpkgs.nix
+    ../../modules/nixos/region.nix
+    ../../modules/nixos/user.nix
+  ];
+
+  home-manager = {
+    extraSpecialArgs = { inherit inputs; };
+    users = {
+      "dmytro-fedkiv" = import ./home.nix;
+    };
+  };
+
+  services.upower.enable = true;
+
+  nixpkgs.config.allowUnfree = true;
+
+  environment.systemPackages = [
+    pkgs.git
+    pkgs.google-chrome
+    pkgs.vscode
+    pkgs.alacritty
+  ];  
+}
