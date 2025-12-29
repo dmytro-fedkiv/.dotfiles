dea3471e5b26745d695e00859ea9f8f283eac7f2
Dmytro Fedkiv <dmytro.fedkiv@icloud.com>
2025-12-29 17:01:16 -0500

    remove redundant components

--- modules/home-manager/desktop/widgets/ags/widget/bar.tsx
+++ modules/home-manager/desktop/widgets/ags/widget/bar.tsx
@@ -3,8 +3,6 @@ import Workspaces from "./workspaces"
 import Clock from "./clock"
 import SystemMenu from "./system-menu"
 import ControlCenter from "./control-center"
-import Power from "./power"
-import Applications from "./applications"
 
 const { TOP, LEFT, RIGHT } = Astal.WindowAnchor
 
@@ -14,12 +12,10 @@ export default function Bar() {
       <centerbox orientation={Gtk.Orientation.HORIZONTAL}>
         <box class="container" $type="start">
           <SystemMenu />
-          <Applications />
           <Workspaces />
         </box>
         <box class="container" $type="center" />
         <box class="container" $type="end">
-          <Power />
           <ControlCenter />
           <Clock />
         </box>
